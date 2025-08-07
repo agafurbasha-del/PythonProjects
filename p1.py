b4499f176b4745b4c0d5d92d97a2e409b1e576ca
agafurbasha-del <agafurbasha@gmail.com>
2025-08-07 17:45:34 +0530

    commit1

--- p1.py
+++ p1.py
@@ -6,3 +6,7 @@ print('address is ',id(a))
 print()
 time.sleep(2)
 print('end of program')
+a, b = 0, 1
+for _ in range(10):
+    print(a)
+    a, b = b, a + b
--- p3.py
+++ p3.py
@@ -6,3 +6,4 @@ a=1000
 print('a value is',a)
 print('data type is',type(a))
 print('address is',id(a))
+print("basha")
